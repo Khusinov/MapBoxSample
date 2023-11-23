ab5ac492be6d4f554756808c256adb1659a4f4ac
usermac <shakhriyordev@gmail.com>
2023-11-23 16:21:35 +0500

    with bugs

--- app/src/main/java/uz/terrabyte/mapboxsample/MainActivity.kt
+++ app/src/main/java/uz/terrabyte/mapboxsample/MainActivity.kt
@@ -1,18 +1,26 @@
 package uz.terrabyte.mapboxsample
 
-import com.mapbox.geojson.Point
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.Bearing
 import com.mapbox.api.directions.v5.models.DirectionsResponse
-import com.mapbox.common.TileStoreOptions.MAPBOX_ACCESS_TOKEN
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.core.constants.Constants.PRECISION_6
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.LineString
+import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.extension.style.layers.generated.lineLayer
@@ -26,6 +34,17 @@ import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
+import com.mapbox.navigation.base.extensions.applyLanguageAndVoiceUnitOptions
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.base.route.NavigationRoute
+import com.mapbox.navigation.base.route.NavigationRouterCallback
+import com.mapbox.navigation.base.route.RouterCallback
+import com.mapbox.navigation.base.route.RouterFailure
+import com.mapbox.navigation.base.route.RouterOrigin
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
+import com.mapbox.navigation.core.lifecycle.requireMapboxNavigation
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,8 +52,12 @@ import uz.terrabyte.mapboxsample.databinding.ActivityMainBinding
 import uz.terrabyte.utills.LocationPermissionHelper
 import java.lang.ref.WeakReference
 
+
 class MainActivity : AppCompatActivity() {
 
+    private val mapboxNavigation: MapboxNavigation by requireMapboxNavigation(
+        onInitialize = this::initNavigation
+    )
     private lateinit var locationPermissionHelper: LocationPermissionHelper
 
     private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
@@ -60,7 +83,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var map: MapboxMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,61 +93,96 @@ class MainActivity : AppCompatActivity() {
 
         locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
         locationPermissionHelper.checkPermissions {
-            onMapReady()
+            onMapReady(mapboxMap = mapView.getMapboxMap())
         }
 
         binding.button.setOnClickListener {
-            drawDirection()
+            fetchARoute()
         }
 
 
     }
 
-    private fun drawDirection() {
-
-        var origin = Point.fromLngLat(-3.588098, 37.176164);
-
-        // Set the destination location to the Plaza del Triunfo in Granada, Spain.
-        var destination = Point.fromLngLat(-3.601845, 37.184080);
-
-
-        val client = MapboxDirections.builder().
-
-        client?.enqueueCall(object : Callback<DirectionsResponse> {
-            override fun onResponse(
-                call: Call<DirectionsResponse>,
-                response: Response<DirectionsResponse>
-            ) {
-
-                if (response.body() == null) {
-                    Log.d(
-                        "No routes found, make sure you set the right user and access token.",
-                        "ddd"
-                    )
-                    return
-                } else if (response.body()!!.routes().size < 1) {
-                    Log.d("No routes found", "ddd")
-                    return
-                }
-
-// Get the directions route
-                val response = response
-
-                val currentRoute = response.body()!!.routes()[0]
-
-            }
-
-            override fun onFailure(call: Call<DirectionsResponse>, throwable: Throwable) {
-
-                Log.d("Error: ", " ${throwable.message} ")
-
-            }
-        })
-
-
+    private fun initNavigation() {
+        MapboxNavigationApp.setup(
+            NavigationOptions.Builder(this)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build()
+        )
     }
 
-    private fun onMapReady() {
+
+    private fun fetchARoute() {
+
+        val originPoint = Point.fromLngLat(
+            41.55758213979963, 60.588573882438624
+        )
+        val destination = Point.fromLngLat(
+            41.550074708879364, 60.58194161940992
+        )
+
+        val routeOptions = RouteOptions.builder()
+// applies the default parameters to route options
+            .applyDefaultNavigationOptions()
+            .applyLanguageAndVoiceUnitOptions(this)
+// lists the coordinate pair i.e. origin and destination
+// If you want to specify waypoints you can pass list of points instead of null
+            .coordinatesList(listOf(originPoint, destination))
+// set it to true if you want to receive alternate routes to your destination
+            .alternatives(false)
+// provide the bearing for the origin of the request to ensure
+// that the returned route faces in the direction of the current user movement
+//            .bearingsList(
+//                listOf(
+//                    Bearing.builder()
+//                        .angle(location.bearing.toDouble())
+//                        .degrees(45.0)
+//                        .build(),
+//                    null
+//                )
+//            )
+            .build()
+        mapboxNavigation.requestRoutes(
+            routeOptions,
+            object : NavigationRouterCallback {
+                override fun onCanceled(routeOptions: RouteOptions, routerOrigin: RouterOrigin) {
+// This particular callback is executed if you invoke
+// mapboxNavigation.cancelRouteRequest()
+//                    binding.responseTextView.text = "route request canceled"
+//                    binding.fetchARouteButton.visibility = VISIBLE
+                }
+
+                override fun onFailure(reasons: List<RouterFailure>, routeOptions: RouteOptions) {
+//                    binding.responseTextView.text = """route request failed with:$reasons
+//""".trimIndent()
+                    Log.e("LOG_TAG", "route request failed with $reasons")
+//                    binding.fetchARouteButton.visibility = VISIBLE
+                }
+
+                override fun onRoutesReady(
+                    routes: List<NavigationRoute>,
+                    routerOrigin: RouterOrigin
+                ) {
+// GSON instance used only to print the response prettily
+                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    val json = routes.map {
+                        gson.toJson(
+                            JsonParser.parseString(it.directionsRoute.toJson())
+                        )
+                    }
+                    mapboxNavigation.setNavigationRoutes(routes)
+
+                    Log.d("TAG", "onRoutesReady: $json")
+                 //   binding.responseTextView.text = """|routes ready (origin: ${routerOrigin::class.simpleName}):|$json""".trimMargin()
+                }
+            }
+        )
+      //  binding.fetchARouteButton.visibility = GONE
+    }
+
+    private fun onMapReady(mapboxMap: MapboxMap) {
+
+        map = mapboxMap
         mapView.getMapboxMap().setCamera(
             CameraOptions.Builder()
                 .zoom(14.0)
@@ -139,12 +197,6 @@ class MainActivity : AppCompatActivity() {
                 )
             ).zoom(ZOOM).build()
         )
-//        mapView.getMapboxMap().loadStyleUri(
-//            Style.MAPBOX_STREETS
-//        ) {
-//            initLocationComponent()
-//            setupGesturesListener()
-//        }
 
         mapView.getMapboxMap().loadStyle(
             (
@@ -159,8 +211,7 @@ class MainActivity : AppCompatActivity() {
                             lineWidth(8.0)
                             lineColor("#888")
                         }
-                    }
-                    )
+                    })
         )
     }
 
